c5aeab87ecc0a53fd32886303d6e66aeeb5935ba
johanperalta12 <johantamayo247@gmail.com>
2026-04-13 12:06:33 -0500

    agregar codigo

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function totalInventario(productos){
-    return productos.reduce((acc,p) => acc + (p.precio * p.stock),0);
-    
+
+function totalInventario(productos) {
+ return productos.reduce((acc, p) => acc + (p.precio * p.stock), 0);
 }
+
+function totalVentas(productos) {
+ return productos.reduce((acc, p) => acc + p.ventas, 0);
+}
+
+function ordenarPorPrecio(productos) {
+ return [...productos].sort((a, b) => a.precio - b.precio);
+}
+
+function buscarProducto(productos, nombre) {
+ return productos.find(p => p.nombre === nombre);
+}
+
