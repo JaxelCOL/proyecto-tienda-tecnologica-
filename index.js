13726f2e94c4932a1e0b721c8bcd896010e7ffa2
Axel Cortes <juliancortes5972@gmail.com>
2026-04-13 12:05:39 -0500

    se agregan las funciones faltantes

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function mostrarProductos(productos) {
+    return productos.map(p => `<p>${p.nombre} - $${p.precio}</p>`).join("");
+}
+
+function stockBajo(productos) { 
+    return productos.filter(p => p.stock < 5 && p.stock > 0); 
+} 
+
+function agotados(productos) { 
+    return productos.filter(p => p.stock === 0); 
+} 
+
+function listaPrecios(productos) { 
+    return productos.map(p => `${p.nombre}: ${p.precio}`); 
+} 
+
 function clasificarPrecio (producto) {
     switch (true){
         case producto.precio < 50000:
